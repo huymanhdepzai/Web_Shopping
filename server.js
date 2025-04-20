360d4f96209ac288aed2574f4c37872dcc4d10af
p3dm <manh.pd225361@sis.hust.edu.vn>
2025-04-21 00:57:42 +0700

    them logic search

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const searchRoute = require('./routes/search');
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'ShopWeb')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'ShopWeb', 'index.html'));
+});
+
+app.use('/search', searchRoute);
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:' + PORT);
+});
