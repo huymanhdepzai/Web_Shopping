0df5e3c92f4c04ff296c7c323526efa5653c9128
p3dm <manh.pd225361@sis.hust.edu.vn>
2025-04-23 11:25:35 +0700

    sua ten file

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const express = require('express');
-const path = require('path');
-const searchRoute = require('./routes/search');
-
+const express = require("express");
+const path = require("path");
+const searchRoute = require("./routes/search");
+const cartRoute = require("./routes/search");
 const app = express();
 
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'ShopWeb')));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, "ShopWeb")));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'ShopWeb', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "ShopWeb", "index.html"));
 });
 
-app.use(express.json());
-app.use('/search', searchRoute);
+app.use("/search", searchRoute);
+app.use("/cart", cartRoute);
 
 app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:' + PORT);
+  console.log("Server is running on http://localhost:" + PORT);
 });
